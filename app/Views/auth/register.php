b5d3cfce1749c05c8ce016aad30add3f4fa96ad6
Hafidzrdwn <hafidz10032005@gmail.com>
2023-06-07 19:49:04 +0700

    menambahkan invalid feedback

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -1,26 +1,28 @@
 <div class="auth-container">
   <div class="auth-header">Daftar Akun Baru</div>
   <i class="fas fa-address-card auth-image"></i>
-  <form action="" class="auth-form">
+  <form id="register-form" action="<?= BASEURL . '/auth/registration'; ?>" method="POST" class="auth-form">
     <div class="form-group">
-      <label for="name">Nama</label>
-      <input type="text" id="name" placeholder="Masukkan nama anda.." name="name">
+      <label for="nama">Nama</label>
+      <input type="text" id="nama" placeholder="Masukkan nama anda.." name="nama">
+      <div class="invalid-feedback"></div>
     </div>
     <div class="form-group">
       <label for="email">Email</label>
-      <input type="email" id="email" placeholder="Masukkan email anda.." name="email">
+      <input type="text" id="email" placeholder="Masukkan email anda.." name="email">
+      <div class="invalid-feedback"></div>
     </div>
-    <div class="form-group pw">
-      <div class="col-1">
-        <label for="password">Password</label>
-        <input type="password" id="password" placeholder="Masukkan password anda.." name="password">
-      </div>
-      <div class="col-2">
-        <label for="confirm_password">Konfirmasi Password</label>
-        <input type="password" id="confirm_password" placeholder="Konfirmasi password anda.." name="confirm_password">
-      </div>
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input type="password" id="password" placeholder="Masukkan password anda.." name="password">
+      <div class="invalid-feedback"></div>
     </div>
-    <button class="auth-btn">Daftar akun</button>
+    <div class="form-group">
+      <label for="konfirmasi_password">Konfirmasi Password</label>
+      <input type="password" id="konfirmasi_password" placeholder="Konfirmasi password anda.." name="konfirmasi_password">
+      <div class="invalid-feedback"></div>
+    </div>
+    <button type="submit" class="auth-btn">Daftar akun</button>
     <div class="auth-small-text">Sudah punya akun? <a href="<?= BASEURL . '/auth/login'; ?>">Masuk</a></div>
   </form>
 </div>
