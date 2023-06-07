f9ae13b6f8707c45ba27af40d9ea391807da4e4b
Hafidzrdwn <hafidz10032005@gmail.com>
2023-06-07 19:34:52 +0700

    menambah resource script baru

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -4,10 +4,14 @@
   </p>
 </footer>
 </div>
+<script>
+  const baseurl = "<?= BASEURL; ?>";
+</script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://kit.fontawesome.com/2e160f1ac0.js" crossorigin="anonymous"></script>
 <script src="<?= BASEURL; ?>/assets/js/script.js?v=<?php echo time(); ?>"></script>
+<script src="<?= BASEURL; ?>/assets/js/auth.js?v=<?php echo time(); ?>"></script>
 </body>
 
 </html>
