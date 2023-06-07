1a60f7facd1a5c44b4081e318447a31f38110455
Hafidzrdwn <hafidz10032005@gmail.com>
2023-06-07 19:35:49 +0700

    membuat method dan sistem registration

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -17,10 +17,40 @@ class AuthController extends Controller
 
   public function registration()
   {
+    $this->middleware->protect_post_url();
+    $dt = FormValidation::make($_POST, [
+      'nama' => 'required|min:3|max:50',
+      'email' => 'required|email',
+      'password' => 'required|min:5',
+      'konfirmasi_password' => 'required|matches:password'
+    ]);
+
+    if ($dt['fails']) {
+      // save errors
+      echo json_encode(['errors' => $dt['errors']]);
+      return;
+    }
+
+    $dt = $dt['data'];
+    $dt['name'] = trim($dt['nama']);
+    $dt['password'] = password_hash($dt['password'], PASSWORD_DEFAULT);
+    $dt['avatar'] = 'default.jpg';
+    $dt['code'] = substr(md5(rand()), 0, 8);
+
+    unset($dt['nama']);
+    unset($dt['konfirmasi_password']);
+
+    if ($insert = $this->model('User')->create($dt)) {
+      echo json_encode(['data' => 'Registrasi Akun Berhasil!', 'code' => $insert['code']]);
+      $_SESSION['isLogin'] = true;
+      $_SESSION['user_auth'] = $insert;
+      exit();
+    }
   }
 
   public function doLogin()
   {
+    $this->middleware->protect_post_url();
   }
 
   public function logout()
