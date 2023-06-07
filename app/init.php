fda45e3fd4fb1ce4c52d16fa2046aef2d580436d
Hafidzrdwn <hafidz10032005@gmail.com>
2023-06-07 19:23:38 +0700

    menambahkan resource helpers dan middlewares

--- app/init.php
+++ app/init.php
@@ -1,7 +1,12 @@
 <?php
+require_once('config.php');
 
 require_once('Core/App.php');
 require_once('Core/Controller.php');
 require_once('Core/Database.php');
 
-require_once('config.php');
+require_once('Middlewares/CustomMiddlewares.php');
+
+spl_autoload_register(function ($class) {
+  require_once('Helpers/' . $class . '.php');
+});
