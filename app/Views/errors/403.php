8486c77195093f8c8c17d4419d01d63e04d64678
Hafidzrdwn <hafidz10032005@gmail.com>
2023-06-15 00:32:30 +0700

    custom url redirect

--- app/Views/errors/403.php
+++ app/Views/errors/403.php
@@ -1,5 +1,8 @@
 <?php
 require_once('../app/views/templates/header.php');
+
+$redirect_url = (isset($_SESSION['isLogin'])) ? BASEURL . '/user/' . $_SESSION['user_auth']['code'] : BASEURL;
+
 ?>
 
 <div class="error-container">
@@ -7,6 +10,6 @@ require_once('../app/views/templates/header.php');
   <div class="error-desc">
     <h1>ERROR <?= $data['judul']; ?>!!</h1>
     <h3>Maaf, Anda tidak memiliki izin untuk mengakses halaman ini.</h3>
-    <a href="<?= BASEURL; ?>">Kembali ke halaman utama</a>
+    <a href="<?= $redirect_url; ?>">Kembali ke halaman utama</a>
   </div>
 </div>
