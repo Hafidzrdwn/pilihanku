5b8b6d54ca70abebb69a1ac8dac20e5ea4f0fb56
Hafidzrdwn <hafidz10032005@gmail.com>
2023-07-03 01:28:42 +0700

    debugging some error if statement

--- app/Helpers/FormValidation.php
+++ app/Helpers/FormValidation.php
@@ -6,35 +6,41 @@ class FormValidation
   public static function make($request, $rules = [])
   {
     $res = [];
+    // mapping request and rules
     foreach ($request as $key => $value) {
       $request[$key] = htmlspecialchars(trim($value));
       if (array_key_exists($key, $rules)) {
         $rules[$key] = explode('|', $rules[$key]);
+      } else {
+        unset($request[$key]);
       }
+    }
 
-      foreach ($rules[$key] as $rule) {
+    // validation process per field
+    foreach ($request as $k => $val) {
+      foreach ($rules[$k] as $rule) {
         switch (true) {
           case $rule == 'required':
-            if (self::isEmpty($value)) $res[$key][] = str_replace('_', ' ', ucwords($key) . " harus diisi, tidak boleh kosong!");
+            if (self::isEmpty($val)) $res[$k][] = str_replace('_', ' ', ucwords($k) . " harus diisi, tidak boleh kosong!");
             break;
           case $rule == 'email':
-            if (!self::isEmail($value)) $res[$key][] = "Email tidak valid!";
+            if (!self::isEmail($val)) $res[$k][] = "Email tidak valid!";
             break;
           case preg_match('/^matches/', $rule):
             $value2 = $request[explode(':', $rule)[1]];
-            if (!self::isMatch($value, $value2)) $res[$key][] = str_replace('_', ' ', ucwords($key) . " tidak cocok!");
+            if (!self::isMatch($val, $value2)) $res[$k][] = str_replace('_', ' ', ucwords($k) . " tidak cocok!");
             break;
           case preg_match('/^min/', $rule):
             $limit = explode(':', $rule)[1];
-            if (!self::checkLength($value, $limit, 'min')) $res[$key][] = 'panjang ' . str_replace('_', ' ', $key . " minimal harus {$limit} karakter!");
+            if (!self::checkLength($val, $limit, 'min')) $res[$k][] = 'panjang ' . str_replace('_', ' ', $k . " minimal harus {$limit} karakter!");
             break;
           case preg_match('/^max/', $rule):
             $limit = explode(':', $rule)[1];
-            if (!self::checkLength($value, $limit, 'max')) $res[$key][] = 'panjang ' . str_replace('_', ' ', $key . " maksimal {$limit} karakter!");
+            if (!self::checkLength($val, $limit, 'max')) $res[$k][] = 'panjang ' . str_replace('_', ' ', $k . " maksimal {$limit} karakter!");
             break;
           case preg_match('/^unique/', $rule):
             $table = explode(':', $rule)[1];
-            if (self::isExist($key, $value, $table)) $res[$key][] = str_replace('_', ' ', $key . " sudah terdaftar!");
+            if (self::isExist($k, $val, $table)) $res[$k][] = str_replace('_', ' ', $k . " sudah terdaftar!");
             break;
         }
       }
